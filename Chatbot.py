721e3a29408c0052f4274ba9b412e4eb391f2046
DHIRAJ SHEWALE <145422324+DHIRAJSHEWALE007@users.noreply.github.com>
2023-12-25 00:10:53 +0530

    Add files via upload

---
+++ Chatbot.py
@@ -0,0 +1,27 @@
+#pip install openai
+
+import openai 
+
+fileopen = open("API.txt","r")
+API = fileopen.read()
+fileopen.close()
+
+openai.api_key = API                                     #providing security to api and chat_log file
+completion =openai.Completion()
+
+def Answer(question):
+    prompt = f'You : {question}\nAssistant : '
+    response = completion.create(
+        model = "text-davinci-002",
+        prompt=prompt,
+        temperature = 0.5,
+        max_tokens = 60,
+        top_p = 0.3,
+        frequency_penalty = 0.5,
+        presence_penalty = 0)
+    ans = response.choices[0].text.strip()
+    return ans
+
+while True:
+     n=input("Query : ")
+     print(Answer(n))
