a81b5b85dbad42e39fb32fd14f0de202098989c2
DHIRAJ SHEWALE <145422324+DHIRAJSHEWALE007@users.noreply.github.com>
2024-04-05 10:07:03 +0530

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -1,27 +1,7 @@
-#pip install openai
-
+#pip install openai==0.28
 import openai 
 
-API = "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"
-openai.api_key = API
-completion =openai.Completion()
-
-def Answer(question):
-    prompt = f'You : {question}\nAssistant : '
-    response = completion.create(
-        model = "gpt-3.5-turbo-instruct",
-        prompt=prompt,
-        temperature = 0.5,
-        max_tokens = 60,
-        top_p = 0.3,
-        frequency_penalty = 0.5,
-        presence_penalty = 0)
-    ans = response.choices[0].text.strip()
-    return ans
-
-while True:
-     n=input("Query : ")
-     print(Answer(n))"
+API = ""
 openai.api_key = API
 completion =openai.Completion()
 
